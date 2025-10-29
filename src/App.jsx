9dc2044ed8d560e012dbb65c95048cc8425d8606
sandipan-Coder <palsandipan68@gmail.com>
2025-10-29 21:31:00 +0530

    Add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import YTPlayer from './components/Player/YTPlayer';
 import Header from './components/Layout/Header';
+import Footer from './components/Layout/Footer';
 
 const App = () => {
   return (
     <div>
       <Header/>
       <YTPlayer/>
+      <Footer />
     </div>
   )
 }
