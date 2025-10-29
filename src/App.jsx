fc601184527a056bbf190084c97842b4624bcfd4
Asfar35 <skasfarali5@gmail.com>
2025-10-29 20:55:27 +0530

    Header Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Header from './components/Layout/Header';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Header/>
   )
 }
 export default App;
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='flex justify-center '>
+      <div className='h-[10vh] w-[85vw] flex justify-between items-center'>
+        <div>Pocket Playlist</div>
+        <div className='flex gap-3'>
+          <button>Video</button>
+          <button>Audio</button>
+        </div>
+      </div>
     </div>
   )
 }
