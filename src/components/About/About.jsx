21590f8c5f67409465a51d343d1f5d606bac7220
Asfar35 <skasfarali5@gmail.com>
2025-11-02 21:42:52 +0530

    Updated About page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,55 @@
 import React from 'react'
 import ProfileCard from '../ui/ProfileCard'
+import Footer from '../Layout/Footer'
 
 const About = () => {
   return (
-    <div className='flex gap-2'>
-      <ProfileCard/>
-      <ProfileCard/>
-      <ProfileCard/>
+    <div>
+      <div className='m-5 flex flex-col flex-center items-center gap-7'>
+        <div className='w-[50vw] text-center'>
+          <h1 className='font-bold text-4xl'>Pocket Playlist : A Background Youtube Music Player Without Ads...</h1>
+        </div>
+        <div className='text-gray-300 w-[55vw] text-center'>
+          We deliver smooth, high-quality background music experiences powered by YouTube. you enjoy the rhythm of creativity that bring music to life.
+        </div>
+      </div>
+    
+
+        <div className='flex justify-center items-center flex-col  p-3 text-center '>
+          <div className='m-10 font-bold text-4xl'>Core Features</div>
+
+          <div className=' flex flex-center items-center gap-5'>
+           <div className='p-7 border-2 border-white rounded-2xl'>
+              <img src="./bg play.jpeg" alt="" className='w-[75px] '/>
+              <div >Backgroud Play</div>
+            </div>
+
+            <div className='p-7 border-2 border-white rounded-2xl'>
+              <img src="./playlist.png" alt="" className='w-[50px] '/>
+              <div >Custom Playlist</div>
+            </div>
+
+            <div className='p-3 border-2 border-white rounded-2xl'>
+              <img src="./ads.png" alt="" className='w-[75px] '/>
+              <div >No Ads.</div>
+            </div>
+          </div>
+        </div>
+
+
+
+      <div className='flex flex-col items-center gap-10'>
+        <div className='font-bold text-4xl'>
+          Meet Our Teams
+        </div>
+        <div className='flex justify-center gap-3'>
+          <ProfileCard Fname="Mostakin Mondal"/>
+          <ProfileCard Fname="Sandipan Pal"/>
+          <ProfileCard Fname="Sk Asfar Ali"/>
+        </div>
+      </div>
+      
+      <Footer/>
     </div>
   )
 }
--- src/components/ui/ProfileCard.jsx
+++ src/components/ui/ProfileCard.jsx
@@ -1,8 +1,8 @@
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
-const ProfileCard = () => {
+const ProfileCard = ({Fname}) => {
   return (
-    <div className="w-80 bg-white rounded-2xl shadow-lg overflow-hidden transition-transform hover:scale-[1.02]">
+    <div className="w-80 text-white bg-[#191622] rounded-2xl shadow-lg overflow-hidden transition-transform hover:scale-[1.02]">
       {/* Header */}
       <div className="h-64 overflow-hidden">
         <img
@@ -14,10 +14,10 @@ const ProfileCard = () => {
 
       {/* Body */}
       <div className="p-6 text-center">
-        <h2 className="text-2xl font-semibold text-gray-800 mb-1">
-          Natalie Paisley
+        <h2 className="text-2xl font-semibold mb-1">
+          {Fname}
         </h2>
-        <p className="text-gray-500 font-medium mb-4">CEO / Co-Founder</p>
+        <p className="text-gray-400 font-medium mb-4">MERN Stack Developer</p>
 
         {/* Footer */}
         <div className="flex justify-center gap-6 text-xl text-gray-600">
