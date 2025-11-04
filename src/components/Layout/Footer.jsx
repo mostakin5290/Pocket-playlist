0faee7523d5d097083532779d222dc1b41a61754
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2025-11-04 20:45:35 +0530

    update footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
               </div>
 
               <nav className="flex flex-col md:flex-row gap-3 md:gap-8 justify-center items-center">
-                <Link to='/about' className="text-sm text-muted-foreground hover:text-foreground transition">About</Link>
+                <Link to='/about' className="text-sm text-muted-foreground hover:text-foreground transition">About us</Link>
                 <Link to="/privacy" className="text-sm text-muted-foreground hover:text-foreground transition">Privacy</Link>
                 <a href="#" className="text-sm text-muted-foreground hover:text-foreground transition">Help</a>
               </nav>
