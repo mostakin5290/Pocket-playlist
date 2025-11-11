e62396c239b15f3c376e395ea15fc91762d2d9f8
Asfar35 <skasfarali5@gmail.com>
2025-11-11 20:06:14 +0530

    Add Privacy Page's Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
 import Layout from './Layout';
 import Search from './components/Search';
-import Privacy from './components/Layout/Privacy';
+import Privacy from './components/Privacy/Privacy';
 
 // import Search from './components/Search';
 
